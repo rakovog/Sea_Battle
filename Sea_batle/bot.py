224f2d53f7c045337e9d9e5dd41f7717b89e8999
Boris Kozhukhovskii <borisgk98@ya.ru>
2022-05-03 18:26:58 +0300

    fix infinity circle and some refactoring

--- Sea_batle/bot.py
+++ Sea_batle/bot.py
@@ -9,23 +9,16 @@ class Bot:
         self.mas_b = self.field.mas_b
         self.size = self.field.size
         self.list = self.field.list
-        self.ship_b = random.choice(self.list)
-        self.ship_b_l = random.randrange(0, self.size)
-        self.ship_b_f = player.get_ship_f(self.ship_b[0])
 
     def xod_bot(self):
         col_ship = self.size * self.size // 3
         for b in range(col_ship):
-
-            bot_set = True
-            while bot_set == 1:
-                for i in range(self.size):
-                    for j in range(self.size):
-                        if i == int(self.ship_b_f) and j == int(self.ship_b_l):
-                            if self.mas_b[i][j] == "*":
-                                bot_set = False
-                                col_ship -= 1
-            for i in range(self.size):
-                for j in range(self.size):
-                    if i == int(self.ship_b_f) and j == int(self.ship_b_l):
-                        self.mas_b[i][j] = "X"
+            ship_x = random.randrange(0, self.size)
+            ship_y = random.randrange(0, self.size)
+            while True:
+                if self.mas_b[ship_x][ship_y] != "*":
+                    ship_x = random.randrange(0, self.size)
+                    ship_y = random.randrange(0, self.size)
+                else:
+                    break
+            self.mas_b[ship_x][ship_y] = "X"
